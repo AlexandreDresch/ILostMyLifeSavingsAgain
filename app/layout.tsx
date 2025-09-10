8eacc600beb654cec4efdab64fcdaca25886fc72
AlexandreDresch <connectWithAlexandre@proton.me>
2025-09-09 23:20:47 -0300

    feat: Enhance styling and theming with Tailwind CSS and add RootProviders

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/root-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <RootProviders>{children}</RootProviders>
         </body>
       </html>
     </ClerkProvider>
